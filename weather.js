85d585b61374ab7abff338b729a0b9c0061aa713
aaron-93 <68589983+aaron-93@users.noreply.github.com>
2020-08-05 01:58:54 +0900

    Add weather.js

---
+++ weather.js
@@ -0,0 +1,37 @@
+const API_KEY = "69736ba302b18dde05fc5ac7f32160af";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+}
+function handleGeoError() {
+  console.log("Can't access geo location.");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCords = localStorage.getItem(COORDS);
+  if (loadedCords === null) {
+    askForCoords();
+  } else {
+    //getWeather
+  }
+}
+function init() {
+  loadCoords();
+}
+
+init();
